97dcd657b848212d55aaa40f3e727163ba20a9c6
Lucie Genevey <l.genevey@gmail.com>
2017-09-11 20:24:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var height = 74
 var message = `${name} is ${height} inches tall`
 var message2 = name + ' is ' + heignt + ' inches tall'
 
-console.log(message)
+console.log(message2)
 
 module.exports = {name, height, message}
